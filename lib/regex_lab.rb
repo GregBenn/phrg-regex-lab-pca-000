b3a8dc5115928aae362f367c09275a6db412fc58
GregBenn <gbenn07@gmail.com>
2018-05-10 08:58:22 -0400

    Regex Lab

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.match(/\A+[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\bun\S*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  # binding.pry
+  text.match(/\A[A-Z]/) && text.match(/[[:punct:]]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+   phone.scan(/\d/).join.length == 10 ? true : false
 end
